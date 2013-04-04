90c6e192de5cf4568fb928bd5422c58280b60403
Evan W. Patton <ewpatton@gmail.com>
2013-04-03 20:02:47 -0400

    Component to triple functionality in Semantic Web component; added LDComponent definition.

---
+++ appinventor/components/src/com/google/appinventor/components/runtime/LDComponent.java
@@ -0,0 +1,52 @@
+package com.google.appinventor.components.runtime;
+
+/**
+ * LDComponent is used to mark {@link AndroidViewComponent}s that provide
+ * linked data features to developers. It is used by the {@link SemanticWeb}
+ * component to generate linked data from forms.
+ * 
+ * @author Evan Patton <ewpatton@gmail.com>
+ *
+ */
+public interface LDComponent {
+  /**
+   * Returns the ConceptURI used to type entries
+   * generated from this component.
+   * @return
+   */
+  public String ConceptURI();
+
+  /**
+   * Sets the ConceptURI used to type entries
+   * generated from this component.
+   * @param uri URI or CURIE of a class on the semantic web,
+   * e.g. http://xmlns.com/foaf/0.1/Person or foaf:Person
+   */
+  public void ConceptURI(String uri);
+
+  /**
+   * Returns the PropertyURI used to link the subject
+   * of a form to the value of this field.
+   * @return
+   */
+  public String PropertyURI();
+
+  /**
+   * Sets the PropertyURI used to link the subject of
+   * a form to the value of this component.
+   * @param uri URI or CURIE of a property on the semantic web,
+   * e.g. http://xmlns.com/foaf/0.1/name or foaf:name
+   */
+  public void PropertyURI(String uri);
+
+  /**
+   * Gets a value of the field in an appropriate form. For example,
+   * checkboxes would return a java.lang.Boolean whereas textboxes
+   * return java.lang.String. The SemanticWeb component will combine
+   * type information with the value provided in {@link #ConceptURI()} to
+   * generate the appropriate linked data elements. 
+   * 
+   * @return
+   */
+  public Object Value();
+}
--- appinventor/components/src/com/google/appinventor/components/runtime/SemanticWeb.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/SemanticWeb.java
@@ -1,11 +1,15 @@
 package com.google.appinventor.components.runtime;
 
 import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Map;
+import java.util.TreeMap;
 
 import com.google.appinventor.components.annotations.DesignerComponent;
 import com.google.appinventor.components.annotations.DesignerProperty;
@@ -22,20 +26,26 @@ import com.google.appinventor.components.common.YaVersion;
 import com.google.appinventor.components.runtime.util.AsyncCallbackPair;
 import com.google.appinventor.components.runtime.util.AsynchUtil;
 import com.google.appinventor.components.runtime.util.WebServiceUtil;
+import com.google.appinventor.components.runtime.util.YailList;
 import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
 import com.hp.hpl.jena.graph.Node;
 import com.hp.hpl.jena.query.Query;
 import com.hp.hpl.jena.query.QueryExecutionFactory;
 import com.hp.hpl.jena.query.QueryFactory;
 import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.rdf.model.Literal;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
 import com.hp.hpl.jena.sparql.core.Var;
 import com.hp.hpl.jena.sparql.engine.binding.Binding;
 import com.hp.hpl.jena.sparql.engine.http.QueryEngineHTTP;
 import com.hp.hpl.jena.vocabulary.RDF;
+import com.hp.hpl.jena.vocabulary.XSD;
 
 import android.util.Log;
 
@@ -52,13 +62,28 @@ import android.util.Log;
 public class SemanticWeb extends AndroidNonvisibleComponent implements
 		Component {
 
+  /* constants for convenience */
   private static final String LOG_TAG = "SemanticWeb";
+  private static final String RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+  private static final String RDFS_NS = "http://www.w3.org/2000/01/rdf-schema#";
+  private static final String OWL_NS = "http://www.w3.org/2002/07/owl#";
 
+  /** endpointURL stores the URI of a SPARQL endpoint **/
   private String endpointURL;
+  /** namespaces stores a mapping of namespace prefixes to their URIs, e.g.
+   * {"foaf:", "http://xmlns.com/foaf/0.1/"}.
+   */
+  private Map<String, String> namespaces = new TreeMap<String, String>();
+  /** baseURL is used for constructing URIs of new objects **/
+  private String baseURL;
 
   public SemanticWeb(ComponentContainer container) {
 	  super(container.$form());
 	  endpointURL = "http://dbpedia.org/sparql";
+	  namespaces.put("rdf", RDF_NS);
+	  namespaces.put("rdfs", RDFS_NS);
+	  namespaces.put("owl", OWL_NS);
+	  baseURL = "http://example.com/";
   }
 
   /**
@@ -80,6 +105,29 @@ public class SemanticWeb extends AndroidNonvisibleComponent implements
   @SimpleProperty
   public void EndpointURL(String url) {
 	  endpointURL = url;
+  }
+
+  /**
+   * Returns the base URL of the component
+   * @see #BaseURL(String)
+   * @return
+   */
+  @SimpleProperty(category = PropertyCategory.BEHAVIOR)
+  public String BaseURL() {
+    return baseURL;
+  }
+
+  /**
+   * Specifies the base URL used for constructing new identifiers
+   * when using linked data components. The URL should end with either
+   * a / or a # per linked data design principles.
+   * @return
+   */
+  @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING,
+      defaultValue = "http://example.com/")
+  @SimpleProperty
+  public void BaseURL(String url) {
+    baseURL = url;
   }
 
   /**
@@ -238,13 +286,25 @@ public class SemanticWeb extends AndroidNonvisibleComponent implements
 
   private Map<String, Model> models = new HashMap<String, Model>();
 
+  /**
+   * Creates a reference to a new semantic web model.
+   * @return
+   */
   @SimpleFunction
   public String OpenModel() {
     Model m = ModelFactory.createDefaultModel();
+    m.setNsPrefixes(namespaces);
     models.put(m.toString(), m);
     return m.toString();
   }
 
+  /**
+   * Loads the contents of the specified path into
+   * the referent model.
+   * @param model Model created using {@link #OpenModel()}
+   * @param path Path to a file containing semantic web data (can be local or HTTP)
+   * @return
+   */
   @SimpleFunction
   public boolean LoadModel(String model, String path) {
     if(!models.containsKey(model)) {
@@ -266,6 +326,13 @@ public class SemanticWeb extends AndroidNonvisibleComponent implements
     return true;
   }
 
+  /**
+   * Adds a triple to the given model.
+   * @param model Model reference created using {@link #OpenModel()}
+   * @param subject URI or CURIE representing the subject of the triple
+   * @param predicate URI or CURIE representing the predicate of the triple
+   * @param object URI or CURIE representing the object of the triple
+   */
   @SimpleFunction
   public void AddObjectTriple(String model, String subject, String predicate, String object) {
     if(!models.containsKey(model)) {
@@ -277,12 +344,18 @@ public class SemanticWeb extends AndroidNonvisibleComponent implements
     Model m = models.get(model);
     Resource s,o;
     Property p;
-    s = m.getResource(subject);
-    p = m.getProperty(predicate);
-    o = m.getResource(object);
+    s = m.getResource(m.expandPrefix(subject));
+    p = m.getProperty(m.expandPrefix(predicate));
+    o = m.getResource(m.expandPrefix(object));
     m.add(s, p, o);
   }
 
+  /**
+   * Saves the model to the given path on the file system.
+   * @param model Model reference created using {@link #OpenModel()}
+   * @param path Path to a local file where the model will be written
+   * @return
+   */
   @SimpleFunction
   public boolean SaveModel(String model, String path) {
     if(!models.containsKey(model)) {
@@ -305,8 +378,175 @@ public class SemanticWeb extends AndroidNonvisibleComponent implements
     return true;
   }
 
+  /**
+   * Closes the model to free up resources.
+   * @param model Model reference from {@link #OpenModel()}
+   */
   @SimpleFunction
   public void CloseModel(String model) {
     models.remove(model);
+  }
+
+  /**
+   * Sets an arbitrary number of (prefix,uri) pairs in the
+   * model that will be used for expanding CURIEs used when
+   * adding data to the model.
+   * @param namespaces A list of pairs mapping prefixes to URIs
+   * @see #SetNamespace(String, String)
+   */
+  @SimpleFunction
+  public void SetNamespaces(YailList namespaces) {
+    @SuppressWarnings("rawtypes")
+    Iterator i = namespaces.iterator();
+    while( i.hasNext() ) {
+      try {
+        YailList pair = (YailList) i.next();
+        if( pair.length() != 2 ) {
+          continue;
+        }
+        final String ns = (String) pair.get(0);
+        final String uri = (String) pair.get(1);
+        SetNamespace( ns, uri );
+      } catch( ClassCastException e ) {
+        // not a nested list...
+      }
+    }
+  }
+
+  /**
+   * Sets a prefix to a specific URI
+   * @param prefix Valid prefix in XML or Turtle
+   * @param uri URI to substitute for the given prefix
+   */
+  @SimpleFunction
+  public void SetNamespace(String prefix, String uri) {
+    if( prefix == null || uri == null ) {
+      return;
+    }
+    namespaces.put(prefix, uri);
+  }
+
+  /**
+   * Lists all of the instances in this model (i.e. all URIs minus Classes and Properties)
+   * @param model Model reference obtained from {@link #OpenModel()}
+   * @return A list of instance URIs in the model
+   */
+  @SimpleFunction
+  public YailList ListInstances(String model) {
+    YailList items = new YailList();
+    /*
+    Model m = models.get(model);
+    if( m != null ) {
+      Iterator<Individual> i = m.listIndividuals();
+      while( i.hasNext() ) {
+        items.add( i.next().getURI() );
+      }
+    }
+    */
+    return items;
+  }
+
+  /**
+   * Retrieves the values for a specific subject,predicate pair in the model.
+   * @param model Model reference obtained from {@link #OpenModel()}
+   * @param subject URI or CURIE representing the subject
+   * @param property URI or CURIE representing the predicate
+   * @return A list of (type, value) pairs where type is either "uri" or "literal" to
+   * indicate whether the value should be interpreted as another node in the graph
+   * or as a fixed literal.
+   */
+  @SimpleFunction
+  public YailList ValuesForProperty(String model, String subject, String property) {
+    YailList values = new YailList();
+    Model m = models.get( model );
+    if( m != null ) {
+      Resource s = m.getResource( m.expandPrefix( subject ) );
+      Property p = m.getProperty( m.expandPrefix( property ) );
+      StmtIterator i = m.listStatements( s, p, (RDFNode) null );
+      while( i.hasNext() ) {
+        Statement stmt = i.next();
+        YailList response = new YailList();
+        try {
+          Resource r = stmt.getResource();
+          response.add( "uri" );
+          response.add( r.getURI() );
+        } catch(Exception e) {
+          Literal l = stmt.getLiteral();
+          response.add( "literal" );
+          response.add( l.getValue() );
+        }
+        if( response.length() == 2 ) {
+          values.add( response );
+        }
+      }
+    }
+    return values;
+  }
+
+  /**
+   * Takes a component implementing the LDComponent interface and uses the properties defined
+   * there to insert a triple into the model using the given subject.
+   * @param component An AndroidViewComponent with a PropertyURI defined
+   * @param model Model reference obtained from {@link #OpenModel()}
+   * @param subject URI or CURIE representing the subject the property,value pair will be added to
+   * @return true if the component was successfully converted into a triple, otherwise false
+   */
+  @SimpleFunction
+  public boolean TriplifyComponentInModel(Component component, String model, String subject) {
+    // check that we can proceed
+    Class<?> clazz = component.getClass();
+    if ( !LDComponent.class.isAssignableFrom(clazz) ) {
+      Log.w(LOG_TAG, "Component does not implement LDComponent");
+      return false;
+    }
+    final LDComponent ldcomponent = (LDComponent) component;
+    final String conceptUri = ldcomponent.ConceptURI();
+    final String propertyUri = ldcomponent.PropertyURI();
+    // verify that propertyUri is set
+    if(propertyUri == null || propertyUri.isEmpty()) {
+      Log.w(LOG_TAG, "Property URI is empty");
+      return false;
+    }
+    final Object value = ldcomponent.Value();
+    // don't store null values
+    if(value == null) {
+      Log.w(LOG_TAG, "Component.value() is null");
+      return false;
+    }
+    Model m = models.get( model );
+    Resource s = m.getResource( m.expandPrefix( subject ) );
+    Property p = m.getProperty( m.expandPrefix( propertyUri ) );
+    if ( conceptUri == null || conceptUri.isEmpty() ) {
+      // if no concept, try to infer xsd datatype
+      if ( value.getClass() == Boolean.class ) {
+        m.add( s, p, value.toString(), XSDDatatype.XSDboolean );
+      } else if ( value.getClass() == Calendar.class ) {
+        Date d = ((Calendar) value).getTime();
+        SimpleDateFormat formatter = new SimpleDateFormat("Y-m-d");
+        m.add( s, p, formatter.format(d), XSDDatatype.XSDdate );
+      } else if ( value.getClass() == String.class ) {
+        m.add( s, p, (String) value );
+      } else {
+        Log.w(LOG_TAG, "Concept URI not supplied and unable to determine appropriate XSD type from Value.getClass");
+        return false;
+      }
+    } else if( conceptUri.startsWith( XSD.getURI() ) ) {
+      // have a concept and it's in the xsd namespace
+      Literal l = m.createTypedLiteral( value.toString(), conceptUri );
+      m.add(s, p, l);
+    } else {
+      // since we are interpreting as a uri, we must have a string
+      if( value.getClass() != String.class ) {
+        Log.w(LOG_TAG, "Have ConceptURI but Value() is not of type java.lang.String");
+        return false;
+      }
+      Resource o = m.getResource( (String) value );
+      // apply the ConceptURI as the type of the value
+      if ( !m.contains( o, RDF.type ) ) {
+        m.add( o, RDF.type, m.getResource( conceptUri ) );
+      }
+      m.add( s, p, o );
+    }
+    return true;
   }
 }
