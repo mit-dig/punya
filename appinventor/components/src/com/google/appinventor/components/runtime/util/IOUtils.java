3fddf5530aaaea0ea2426119ff014727038717a1
Evan W. Patton <ewpatton@mit.edu>
2020-10-02 15:26:53 -0400

    Implement input stream to string reader
    Change-Id: I81ff1137e9f81d86af265fb7ed4399e0fdddba00

--- appinventor/components/src/com/google/appinventor/components/runtime/util/IOUtils.java
+++ appinventor/components/src/com/google/appinventor/components/runtime/util/IOUtils.java
@@ -5,10 +5,12 @@
 
 package com.google.appinventor.components.runtime.util;
 
+import java.io.ByteArrayOutputStream;
 import java.io.Closeable;
 import java.io.IOException;
 
 import android.util.Log;
+import java.io.InputStream;
 
 public final class IOUtils {
   /**
@@ -22,5 +24,23 @@ public final class IOUtils {
     } catch (IOException e) {
         Log.w(tag, "Failed to close resource", e);
     }
+  }
+
+  /**
+   * Reads the contents of a stream into a String, using the default charset
+   * (UTF-8 on Android).
+   *
+   * @param stream the stream of data to read
+   * @return the stream contents interpreted as UTF-8
+   * @throws IOException if the stream cannot be read
+   */
+  public static String readStream(InputStream stream) throws IOException {
+    byte[] chunk = new byte[4096];
+    int read;
+    ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+    while ((read = stream.read(chunk)) > 0) {
+      buffer.write(chunk, 0, read);
+    }
+    return buffer.toString();
   }
 }
