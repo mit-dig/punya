93b69bc2d53ddb2c830006d83d83846ccac77402
Evan W. Patton <ewpatton@mit.edu>
2020-10-27 08:32:48 -0400

    Bump companion version to 2.58punya2
    Change-Id: I801a6a7523a591cbee392179a363a06a2253a526

--- appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
+++ appinventor/components/src/com/google/appinventor/components/common/YaVersion.java
@@ -1543,11 +1543,11 @@ public class YaVersion {
 
   public static final String ACCEPTABLE_COMPANION_PACKAGE = "edu.mit.appinventor.punya.aicompanion3";
 
-  public static final String PREFERRED_COMPANION = "2.58punya1a";
+  public static final String PREFERRED_COMPANION = "2.58punya2";
   public static final String COMPANION_UPDATE_URL = "";
   public static final String COMPANION_UPDATE_URL1 = "";
   public static final String COMPANION_UPDATE_EMULATOR_URL = "";
-  public static final String [] ACCEPTABLE_COMPANIONS = { "2.58punya1a", "2.58punya1au" };
+  public static final String [] ACCEPTABLE_COMPANIONS = { "2.58punya2", "2.58punya2u" };
 
   // Splash Screen Values
   public static final int SPLASH_SURVEY = 1;
